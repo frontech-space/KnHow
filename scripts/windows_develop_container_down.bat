ea88237e0fd58641759ded3c484f3eb24e877aa8
Memolia <ultimum.memolia@gmail.com>
2025-02-06 19:39:47 +0900

    Docker操作バッチファイル作成

---
+++ scripts/windows_develop_container_down.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001 > nul
+setlocal enabledelayedexpansion
+
+rem 開発環境用
+
+rem スクリプトの場所を取得
+set SCRIPT_DIR=%~dp0
+
+rem プロジェクトのルートディレクトリを設定
+set PROJECT_ROOT=%SCRIPT_DIR%..
+
+rem 現在の作業ディレクトリをプロジェクトルートに変更
+cd /d %PROJECT_ROOT%
+
+rem ビルドから起動したい時に使用 .envの設定に注意
+docker compose down
