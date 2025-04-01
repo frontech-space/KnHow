88f0f98ce33bf5b5593cd4e0d98454542efa606f
Memolia <ultimum.memolia@gmail.com>
2025-04-01 20:19:31 +0900

    ディレクトリ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <main className="container mx-auto px-4 py-8">
+        <Routes>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
