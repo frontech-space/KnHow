a15dabe1745a669ae99e2dc02f612d731c889acc
Memolia <ultimum.memolia@gmail.com>
2025-05-20 00:34:47 +0900

    gh-pages変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Top from "./pages/top";
-import About from "./pages/about";
-import Release from "./pages/release";
-import Contact from "./pages/contact";
 
 const App = () => {
   return (
     <HelmetProvider>
-      <Router basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Top />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/release" element={<Release />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </HelmetProvider>
   );
 };
