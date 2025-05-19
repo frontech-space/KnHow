7103be7d9c532666eb19b58958d6be93e662e00c
Memolia <ultimum.memolia@gmail.com>
2025-05-19 18:30:38 +0900

    Actions作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Top from "./pages/top";
 import About from "./pages/about";
@@ -6,16 +6,22 @@ import Release from "./pages/release";
 import Contact from "./pages/contact";
 
 const App = () => {
+  // GitHub Pagesの場合はbasenameを設定、それ以外は空文字
+  const basename =
+    process.env.REACT_APP_DEPLOY_ENV === "github-pages"
+      ? process.env.PUBLIC_URL
+      : "";
+
   return (
     <HelmetProvider>
-      <BrowserRouter>
+      <Router basename={basename}>
         <Routes>
           <Route path="/" element={<Top />} />
           <Route path="/about" element={<About />} />
           <Route path="/release" element={<Release />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </HelmetProvider>
   );
 };
