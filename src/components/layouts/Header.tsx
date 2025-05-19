8363322395641590c2fcb3cbc0b20863ef689dbe
Memolia <ultimum.memolia@gmail.com>
2025-05-20 01:19:06 +0900

    gh-pages変更

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { HeaderProps } from "../../types/layout";
 import Image from "../common/Image";
+import { getAssetPath } from "../../utils/path";
 
 const Header = ({ title }: HeaderProps) => {
   return (
@@ -11,7 +12,7 @@ const Header = ({ title }: HeaderProps) => {
           <div className="w-1/2 md:w-1/3 flex justify-start">
             <Link to="/" className="flex items-center">
               <Image
-                src="/assets/images/knhow-panda-icon.png"
+                src={getAssetPath("/assets/images/knhow-panda-icon.png")}
                 alt="KnHow"
                 className="h-20 md:h-24 w-auto"
               />
