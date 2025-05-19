5ae32b33965cc6bd987f9f960a158124c66f9c9f
栗林寛人 <kuribayashihiroto@kuribayashihirotonoMacBook-Pro.local>
2025-05-19 18:19:26 +0900

    もっと見るボタンの追加

--- src/pages/release.tsx
+++ src/pages/release.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Layout from "../components/layouts/Layout";
 import Text from "../components/common/Text";
 import ReleaseItem from "../components/features/release/ReleaseCards";
+import Button from "../components/common/Button";
 
 const Release = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const releaseItems: { version: string; feature: string }[] = [
     { version: "Ver1.0", feature: "-ノウハウ共有機能" },
@@ -13,17 +17,47 @@ const Release = () => {
     { version: "Ver1.0", feature: "-ノウハウ共有機能" },
   ];
 
+    const visibleItems = isExpanded ? releaseItems : releaseItems.slice(0, 4);
+
   return (
     <Layout title="リリースノート">
       <div className="flex flex-col md:flex-row w-full min-h-screen">
-        <section className="w-full md:w-[80%] flex flex-wrap justify-center">
-          {releaseItems.map((releaseItem, index) => (
-              <ReleaseItem 
-                key={index}
-                version={releaseItem.version} 
-                feature={releaseItem.feature} 
-              />
-            ))}
+        <section className="w-full md:w-[80%] flex flex-wrap justify-center md:hidden">
+          {visibleItems.map((item, index) => (
+            <ReleaseItem
+              key={`mobile-${index}`}
+              version={item.version}
+              feature={item.feature}
+            />
+          ))}
+
+          <div className="w-full flex justify-center mt-4">
+            {!isExpanded && releaseItems.length > 4 && (
+              <Button
+                size="medium"
+                backgroundColor="primary"
+                borderColor="tertiary"
+                textColor="white"
+                hoverColor="opacity"
+                isRound={true}
+                isOutline={true}
+                onClick={() => setIsExpanded(true)}
+                className="w-fit px-[5%] py-[1%] mx-[1%]"
+              >
+                もっと見る
+              </Button>
+            )}
+          </div>
+        </section>
+
+        <section className="hidden md:flex md:w-[80%] flex-wrap justify-center">
+          {releaseItems.map((item, index) => (
+            <ReleaseItem
+              key={`pc-${index}`}
+              version={item.version}
+              feature={item.feature}
+            />
+          ))}
         </section>
         <div className="border-r-2 border-black opacity-40 self-stretch mx-2"></div>
         <aside className="w-full md:w-[18%] py-[2%]">
